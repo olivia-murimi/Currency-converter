e7699624a901bb13669d99c12fdeb85deed0ff65
Olivia Waithira <oliviawaithira7@gmail.com>
2024-08-24 23:48:45 +0300

    javascript

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const currencyFirstEl = document.getElementById("currency-first");
 
 const worthFirstEl = document.getElementById("worth-first")
+
+const currencySecondEl = document.getElementById("currency-sirst");
+
+const worthSecondEl = document.getElementById("worth-second")
